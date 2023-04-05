f09fc705b5af9b4618dd4f0707ff06b0e679bb8c
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-05 15:24:54 +0200

    commenti

--- js/main.js
+++ js/main.js
@@ -3,22 +3,22 @@ console.log ("hello console");
 
 /*nome utente generato tramite prompt e richiamato in html tramite id e segnalato in console*/
 let nomeUtente = prompt("Inserisci il tuo nome!");
-document.getElementById("nome").innerText = nomeUtente;
+/*document.getElementById("nome").innerText = nomeUtente;*/
 console.log (`L'utente si chiama ${ nomeUtente } `);
 
 /*cognome utente generato tramite prompt e richiamato in html tramite id e segnalato in console*/
 let cognomeUtente = prompt ("Inserisci il tuo cognome!");
-document.getElementById("cognome").innerText = cognomeUtente;
+/*document.getElementById("cognome").innerText = cognomeUtente;*/
 console.log (`Cognome Utente ${ cognomeUtente } `);
 
 /*colore utente generato tramite prompt e richiamato in html tramite id e segnalato in console*/
 let coloreUtente = prompt ("Colore preferito");
-document.getElementById ("colore").innerText = coloreUtente;
+/*document.getElementById ("colore").innerText = coloreUtente;*/
 console.log (`Colore preferito Utente ${ coloreUtente } `);
 
 /* creazione del numero usando una variabile */
 let annoCorrente = Number (2023);
 
 /*Creazione pw mettendo insieme tutte le variabili richiamate in html usando id e generate in console*/
-document.getElementById ("pw").innerText = ` ${nomeUtente} ${cognomeUtente} ${coloreUtente} ${annoCorrente} `;
-console.log (`pw = ${ coloreUtente } ${cognomeUtente} ${coloreUtente} ${annoCorrente} `);
+document.getElementById ("pw").innerText = ` ${nomeUtente}- ${cognomeUtente}- ${coloreUtente}- ${annoCorrente} `;
+console.log (`pw = ${ coloreUtente }- ${cognomeUtente}- ${coloreUtente}- ${annoCorrente} `);
